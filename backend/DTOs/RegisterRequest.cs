cb44604867afdf3e8fabf054068abe53214e70ae
minthakasandul <IT23372580@my.sliit.lk>
2025-09-10 18:47:14 +0530

    Implemented user registration and login backend

---
+++ backend/DTOs/RegisterRequest.cs
@@ -0,0 +1,12 @@
+﻿namespace HospitalManagementSystem.DTOs
+{
+    public class RegisterRequest
+    {
+        public string FullName { get; set; }
+        public string DOB { get; set; }     // Keep DOB as string, will parse later
+        public string Email { get; set; }
+        public string Password { get; set; }
+        public string Contact { get; set; }
+        public string Role { get; set; }    // Admin, Doctor, Patient, Vendor
+    }
+}
