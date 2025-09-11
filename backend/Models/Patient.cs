5eae54f300db751c99b0ac6f2d4ce87ddf3091ea
minthakasandul <IT23372580@my.sliit.lk>
2025-09-11 23:41:41 +0530

    Implement register patient backend

---
+++ backend/Models/Patient.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace HospitalManagementSystem.Models
+{
+    public class Patient
+    {
+        public string? PatientId { get; set; }   // not required (controller generates it)
+        public string UserId { get; set; }       // required
+        public string Name { get; set; }         // required
+        public DateTime Dob { get; set; }        // required
+        public string Gender { get; set; }       // required
+        public string Contact { get; set; }      // required
+        public string? Age { get; set; }         // not required (controller calculates it)
+    }
+}
