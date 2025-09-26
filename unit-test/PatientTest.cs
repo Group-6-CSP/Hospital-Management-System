ab09956f2d192286afa754ee8f59219ade39e3ed
chanuka0 <it23372658@my.sliit.lk>
2025-09-26 22:56:30 +0530

    unit test for patient

---
+++ unit-test/PatientTest.cs
@@ -0,0 +1,15 @@
+﻿namespace unit_test;
+
+public class patient.test
+{
+    [SetUp]
+    public void Setup()
+    {
+    }
+
+    [Test]
+    public void Test1()
+    {
+        Assert.Pass();
+    }
+}
