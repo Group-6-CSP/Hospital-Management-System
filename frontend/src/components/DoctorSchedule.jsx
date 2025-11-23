669c4e05d05caee301b1ca94ac786ebbbdff4b10
minthakasandul <IT23372580@my.sliit.lk>
2025-11-23 20:34:12 +0530

    Fix React ESLint warnings for Azure build

--- frontend/src/components/DoctorSchedule.jsx
+++ frontend/src/components/DoctorSchedule.jsx
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from 'react';
 //import { getDoctorSchedule, updateDoctorSchedule } from '../services/doctorService';
+import { updateDoctorSchedule } from "../services/doctorService";
 import axios from 'axios';
 
 function DoctorSchedule() {
